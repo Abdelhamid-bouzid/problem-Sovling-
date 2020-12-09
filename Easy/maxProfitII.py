994db4124cbc6120389eeefc6341c8f382e7f6ca
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2020-12-08 23:52:13 -0500

    Update maxProfitII.py

--- Easy/maxProfitII.py
+++ Easy/maxProfitII.py
@@ -1,6 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxprofit=0
+        vally = prices[0]
+        peak  = prices[0]
         i=0
         while i<len(prices)-1:
             while i<len(prices)-1 and prices[i]>=prices[i+1]:
