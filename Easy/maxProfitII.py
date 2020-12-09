0115c43fabf26f0d8fe3976b19527565935be28f
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2020-12-08 23:47:23 -0500

    Create maxProfitII.py

---
+++ Easy/maxProfitII.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxprofit=0
+        i=0
+        while i<len(prices)-1:
+            while i<len(prices)-1 and prices[i]>=prices[i+1]:
+                i+=1
+                
+            vally = prices[i]
+            while i<len(prices)-1 and prices[i]<=prices[i+1]:
+                i+=1
+                
+            peak      = prices[i]
+            maxprofit += peak-vally
+            
+        return maxprofit
