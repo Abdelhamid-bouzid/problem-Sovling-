b5e421f34ca2b7b26d6da0e9be7675d89a4ab7c8
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2020-12-14 20:31:42 -0500

    Create titleToNumber.py

---
+++ Easy/titleToNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        result = 0;  
+        for i in range(len(s)):  
+            result *= 26;  
+            result += ord(s[i]) - ord('A') + 1;  
+            
+        return result
