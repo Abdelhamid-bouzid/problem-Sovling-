633a1f6376185ad2329a417b63533c3a4c10a094
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2020-12-26 18:35:52 -0500

    Create letterCombinations.py

---
+++ Medium/letterCombinations.py
@@ -0,0 +1,47 @@
+'''
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+'''
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}    
+        result = []
+        
+        def helpCombine(current, leftoverDigits):
+            if not leftoverDigits:
+                result.append(current)
+                return 
+            else:
+                for char in phone[leftoverDigits[0]]:
+                    helpCombine(current + char, leftoverDigits[1:])
+        
+        if not digits:
+            return []
+        else: 
+            helpCombine("", digits)
+            return result
