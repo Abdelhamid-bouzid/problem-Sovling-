43bde9393df3d86f9f3041f712e18b69be877f4a
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2020-12-28 17:39:58 -0500

    Create search.py

---
+++ Medium/search.py
@@ -0,0 +1,30 @@
+'''
+You are given an integer array nums sorted in ascending order, and an integer target.
+
+Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+If target is found in the array return its index, otherwise, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
+        
