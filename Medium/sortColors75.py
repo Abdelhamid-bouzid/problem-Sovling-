cc79c74e2e6044536630464c992cfdd66d7cc896
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-01-18 23:21:54 -0500

    Create sortColors75.py

---
+++ Medium/sortColors75.py
@@ -0,0 +1,40 @@
+'''
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+'''
+from collections import Counter
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        count = Counter(nums)
+        for i in range(len(nums)):
+            if i < count[0]:
+                nums[i] = 0
+            elif i < count[0] + count[1]:
+                nums[i] = 1
+            else:
+                nums[i] = 2
+        
