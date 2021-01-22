bbd4b1909b096606a8ce22d0db77bf0600d8b4c5
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-01-21 20:28:20 -0500

    Update merge2Lists23.py

--- Hard/merge2Lists23.py
+++ Hard/merge2Lists23.py
@@ -32,14 +32,21 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        amount = len(lists)
-        interval = 1
-        while interval < amount:
-            for i in range(0, amount - interval, interval * 2):
-                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
-            interval *= 2
-        return lists[0] if amount > 0 else None
-
+        if len(lists)==0:
+            return None
+        
+        while len(lists)>1:
+            lists = self.mergr1level(lists)
+        return lists[0]
+    
+    def mergr1level(self,lists):
+        n_list = []
+        for i in range(0,len(lists)-1,2):
+            n_list.append(self.merge2Lists(lists[i], lists[i + 1]))
+        if len(lists)%2!=0:
+            n_list.append(lists[-1])
+        return n_list
+        
     def merge2Lists(self, l1, l2):
         head = point = ListNode(0)
         while l1 and l2:
