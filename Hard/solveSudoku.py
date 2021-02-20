38070ceb039c08fd5ef478ade9736910c53bc081
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-02-20 09:23:07 -0500

    Create solveSudoku.py

---
+++ Hard/solveSudoku.py
@@ -0,0 +1,46 @@
+'''
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+'''
+class Solution:
+    def solveSudoku(self, board):
+        def isValid(x,y):
+            tmp=board[x][y]; board[x][y]='D'
+            for i in range(9):
+                if board[i][y]==tmp: return False
+            for i in range(9):
+                if board[x][i]==tmp: return False
+            for i in range(3):
+                for j in range(3):
+                    if board[int(x/3)*3+i][int(y/3)*3+j]==tmp: return False
+            board[x][y]=tmp
+            return True
+        def dfs(board):
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j]=='.':
+                        for k in '123456789':
+                            board[i][j]=k
+                            if isValid(i,j) and dfs(board):
+                                return True
+                            board[i][j]='.'
+                        return False
+            return True
+        dfs(board)
