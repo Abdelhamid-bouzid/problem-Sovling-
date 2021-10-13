02b8cb42b7149073dc34eb87ecf1e92f6566a936
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-10-13 16:32:10 -0400

    Create findCenter.py

---
+++ Easy/findCenter.py
@@ -0,0 +1,37 @@
+'''
+1791. Find Center of Star Graph
+Easy
+
+248
+
+41
+
+Add to List
+
+Share
+There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+
+You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+
+ 
+
+Example 1:
+
+
+Input: edges = [[1,2],[2,3],[4,2]]
+Output: 2
+Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
+Example 2:
+
+Input: edges = [[1,2],[5,1],[1,3],[1,4]]
+Output: 1
+'''
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        a,b = edges[0][0],edges[0][1]
+        if a == edges[1][0] or a==edges[1][1]:
+            star = a
+        else:
+            star = b
+        return star  
+            
