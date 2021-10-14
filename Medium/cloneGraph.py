16a74f4e0069f7be716d3b17a1336efafe4a60a8
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-10-14 14:09:02 -0400

    Create cloneGraph.py

---
+++ Medium/cloneGraph.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node):
+        
+        self.visited = {}
+        return self.help(node)
+        
+    def help(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return node
+
+        # If the node was already visited before.
+        # Return the clone from the visited dictionary.
+        if node in self.visited:
+            return self.visited[node]
+
+        # Create a clone for the given node.
+        # Note that we don't have cloned neighbors as of now, hence [].
+        clone_node = Node(node.val, [])
+
+        # The key is original node and value being the clone node.
+        self.visited[node] = clone_node
+
+        # Iterate through the neighbors to generate their clones
+        # and prepare a list of cloned neighbors to be added to the cloned node.
+        if node.neighbors:
+            clone_node.neighbors = [self.help(n) for n in node.neighbors]
+
+        return clone_node
